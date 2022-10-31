f182c1917d3d599ee466ce460311284809072b1e
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-31 22:59:16 +0000

    saved

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import share from './icons/share.png';
 import dotShare from './icons/dot.png';
 import camera from './icons/camera.svg';
+import git from './icons/git.svg';
+import slak from './icons/slack.svg';
+import good from './icons/good.svg';
 import profile from './images/wearingglass.png'
 
 function App() {
@@ -45,13 +48,34 @@ function App() {
           </div>
         </section>
         <div>
-          <a>
-           <img src=''/>
+          <a href='#' style={{padding:'1rem'}}>
+           <img src={slak} alt='slack'/>
+          </a>
+          <a href='#'style={{padding:'1rem'}}>
+           <img src={git} alt='git'/>
           </a>
         </div>
       </div>
       <footer className='link-tree-footer'>
+        <div className='link-tree-footer-content'>
 
+      <div style={{display: 'flex', alignItems:'center'}}>
+     <h2> Zuri </h2><span style={{background: "#e7332b",
+borderRadius: "50%",
+display: 'inline-block',
+height: '8px',
+width: '8px',
+margin: '0 .5rem'}}></span> 
+<h2>Internship</h2>
+      </div>
+      <div style={{color:'grey'}}>
+      HNG Internship 9 Frontend Task
+      </div>
+      <div>
+        <img src={good} alt='food logo'/>
+      </div>
+      
+      </div>
       </footer>
       </div>
      </main>

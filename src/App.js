c42fb3e980d14eca2c67cfabd1319d8875a51c51
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-31 22:41:54 +0000

    svg images

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className='lintree-wrapper'>
         <section className='linktree-header'>
         <div className='share'>
-          <span><img src={share} alt='share'/></span>
+          <span className='share-icon-upper'><img src={share} alt='share'/></span>
           <span><img src={dotShare} alt='dot-btn'/></span>
         </div>
         <div className='profile-wrapper'>
@@ -23,11 +23,36 @@ function App() {
             <span className='camera'>
             <img src={camera} alt='camera' style={{color:'transparent'}}/>
             </span>
-           
            </div>
+           <h2 id='twitter'>romauld</h2>
+           <div id='slack' style={{display: 'none'}}>romauld</div>
         </div>
         </section>
+        <section className='lintree-section'>
+          <div className='lintree-wrap'>
+          <div className='linktree-content'>
+            <div>
+            
+              <a href='#'  className='link-items'>
+              <span></span>
+                Twitter
+              <span>
+                <img src={share} alt='share inner' className='share-icon'/>
+              </span>
+                </a>
+            </div>
+          </div>
+          </div>
+        </section>
+        <div>
+          <a>
+           <img src=''/>
+          </a>
+        </div>
       </div>
+      <footer className='link-tree-footer'>
+
+      </footer>
       </div>
      </main>
     </div>

8239d4c26cd284b2f56bbe9969d381e65ac29aa5
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-04 18:27:42 +0000

    form header

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 export default function Contacts() {
   return (
-    <div>Coontacts</div>
+    <div className='form-container'>
+        <div className='form-inner'>
+        <div className='form-wrapper'>
+            <div className='form-cont'>
+                <div className='form-content'>
+                <section className='header'>
+                    <div className='contact-header'>Contact Me</div>
+                    <div className='support'>Hi there, contact me to ask me about anything you have in mind</div>
+                </section>
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </div>
   )
 }
 
